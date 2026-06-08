0cdbab2350fbf3226c60280eef1b943e3dda579f
Peter Kiprop <PKiprop@Caldwell.edu>
2026-06-08 08:03:24 +0000

    ${8AM4:03 AMe4:03 AMi3e}

--- app.py
+++ app.py
@@ -2,3 +2,7 @@ import streamlit as st
 import yfinance as yf
 import plotly.graph_objects as go
 import pandas as pd
+
+st.set_page_config(page_title="Stock Dashboard", layout="Wide")
+st.title("Stock Comparison Dashboard")
+st.write("AAPL . GOOGL . META")
